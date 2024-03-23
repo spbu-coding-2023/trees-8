ed08a17690f0f8e1e0fee799594eb169baa1a08f
odiumuniverse <st116743@student.spbu.ru>
2024-03-23 16:47:53 +0300

    Change `remove` method and add exception in `set` method

--- lib/src/main/kotlin/bstrees/implementations/SimpleTree.kt
+++ lib/src/main/kotlin/bstrees/implementations/SimpleTree.kt
@@ -3,7 +3,7 @@ package bstrees.implementations
 import bstrees.templates.BSTreeTemplate
 
 class SimpleTree<K : Comparable<K>, V> : BSTreeTemplate<K, V, SimpleVertex<K, V>>() {
-    
+
     override fun remove(key: K): V? {
         val vertex = vertByKey(key) ?: return null
         val parent = vertex.parent
@@ -37,7 +37,40 @@ class SimpleTree<K : Comparable<K>, V> : BSTreeTemplate<K, V, SimpleVertex<K, V>
     }
 
     override operator fun set(key: K, value: V): V? {
-        // set implemenation
-        return null
+
+        if (root == null) {
+            val vertex = SimpleVertex(key, value)
+            root = vertex
+            size += 1
+            return null
+        }
+
+        var current = root ?: throw IllegalStateException("Can't be null, there was a check above")
+        while (true) {
+            val result = current.key.compareTo(key)
+            if (result < 0) {
+                if (current.left == null) {
+                    val newVertex = SimpleVertex(key, value)
+                    current.left = newVertex
+                    newVertex.parent = current
+                    size += 1
+                    return null
+                }
+                current = current.left ?: throw IllegalStateException("Case when cur.left is null is processed above")
+            } else if (result > 0) {
+                if (current.right == null) {
+                    val newVertex = SimpleVertex(key, value)
+                    current.right = newVertex
+                    newVertex.parent = current
+                    size += 1
+                    return null
+                }
+                current = current.right ?: throw IllegalStateException("Case when cur.right is null is processed above")
+            } else {
+                val oldValue = current.value
+                current.value = value
+                return null
+            }
+        }
     }
 }
