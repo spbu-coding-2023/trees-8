1cb7bc19bf37f9a731eac2281bda3508f82503df
Alexei Dmitrievtsev <dmt.alex2005@gmail.com>
2024-03-26 11:03:43 -0400

    add: RBTree right/left rotation

--- lib/src/main/kotlin/bstrees/implementations/RBTree.kt
+++ lib/src/main/kotlin/bstrees/implementations/RBTree.kt
@@ -8,6 +8,52 @@ class RBTree<K : Comparable<K>, V> : BalanceBSTreeTemplate<K, V, RBVertex<K, V>>
         //balance(currentVert)
         return returnResult
     }
+    private fun rotateRight(vertex: RBVertex<K, V>) {
+        val parent = vertex.parent
+        val leftChild = vertex.left
+
+        vertex.left = leftChild?.right
+        if (leftChild?.right != null) {
+            leftChild.right?.parent = vertex
+        }
+        leftChild?.right = vertex
+        vertex.parent = leftChild
+        if (parent == null) {
+            root = leftChild
+        }
+        else if (parent.left == vertex) {
+            parent.left = leftChild
+        }
+        else if (parent.right == vertex) {
+            parent.right = leftChild
+        }
+        if (leftChild != null) {
+            leftChild.parent = parent;
+        }
+    }
+    private fun rotateLeft(vertex: RBVertex<K, V>) {
+        val parent  = vertex.parent
+        val rightChild = vertex.right
+        vertex.right = rightChild?.left
+        if (rightChild?.left != null) {
+            rightChild.left?.parent = vertex
+        }
+        rightChild?.left = vertex
+        vertex.parent = rightChild
+        if (parent == null) {
+            root = rightChild
+        }
+        else if (parent.left == vertex) {
+            parent.left = rightChild
+        } else if (parent.right == vertex) {
+            parent.right = rightChild
+        }
+        if (rightChild != null) {
+            rightChild.parent = parent;
+        }
+    }
+
+
     private fun unbalancedSet(key: K, value: V): Pair<RBVertex<K, V>, V?> {
         if (root == null){
             val newVertex = RBVertex(key, value)
