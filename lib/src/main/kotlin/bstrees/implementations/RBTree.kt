51d137aeca46773e3726419c6083da1dcd88c8b0
OGchukh <dmt.alex2005@gmail.com>
2024-03-26 07:31:00 -0400

    add: unbalanced set for RBTree

--- lib/src/main/kotlin/bstrees/implementations/RBTree.kt
+++ lib/src/main/kotlin/bstrees/implementations/RBTree.kt
@@ -1,3 +1,49 @@
 package bstrees.implementations
 
-class RBTree
+import bstrees.templates.BalanceBSTreeTemplate
+
+class RBTree<K : Comparable<K>, V> : BalanceBSTreeTemplate<K, V, RBVertex<K, V>>() {
+    override fun set(key: K, value: V): V? {
+        val (currentVert, returnResult) = unbalancedSet(key, value)
+        //balance(currentVert)
+        return returnResult
+    }
+    private fun unbalancedSet(key: K, value: V): Pair<RBVertex<K, V>, V?> {
+        if (root == null){
+            val newVertex = RBVertex(key, value)
+            root = newVertex
+            size += 1
+            return Pair(newVertex, null)
+        }
+        var currentVertex = root ?: throw IllegalStateException()
+        while (true){
+            val result = currentVertex.key.compareTo(key)
+            if (result < 0){
+                if(currentVertex.left == null){
+                    val newVertex = RBVertex(key, value)
+                    currentVertex.left = newVertex
+                    newVertex.parent = currentVertex
+                    size+= 1
+                    return Pair(newVertex, null)
+                }
+                currentVertex = currentVertex.left ?: throw IllegalStateException()
+            } else if (result > 0){
+                if (currentVertex.right == null){
+                    val newVertex = RBVertex(key, value)
+                    currentVertex.right = newVertex
+                    newVertex.parent = currentVertex
+                    size += 1
+                    return Pair(newVertex, null)
+                }
+                currentVertex = currentVertex.right ?: throw IllegalStateException()
+            } else{
+                val oldValue = currentVertex.value
+                currentVertex.value = value
+                return Pair(currentVertex, oldValue)
+            }
+        }
+    }
+    override fun remove(key: K): V?{
+        return null
+    }
+}
