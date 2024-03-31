09aca2bbe2e5a00a28d7be4842d17b20a3ba4eed
Gleb Nasretdinov <gleb.romashkov@gmail.com>
2024-03-31 20:35:59 +0300

    Renamed all occurrences of AVLTree to AvlTree

--- lib/src/main/kotlin/trees/implementations/AvlTree.kt
+++ lib/src/main/kotlin/trees/implementations/AvlTree.kt
@@ -2,7 +2,7 @@ package trees.implementations
 
 import trees.templates.BalanceBSTreeTemplate
 
-class AVLTree<K : Comparable<K>, V> : BalanceBSTreeTemplate<K, V, AVLVertex<K, V>>() {
+class AvlTree<K : Comparable<K>, V> : BalanceBSTreeTemplate<K, V, AVLVertex<K, V>>() {
     public override operator fun set(key: K, value: V): V? {
         val (currentVert, oldValue) = setWithoutBalance(key, value)
         if (oldValue == null) {
