4757da0d00d9ed216d6add9346299d1ba4d323ae
odiumuniverse <st116743@student.spbu.ru>
2024-04-01 02:25:39 +0300

    Add tests on RB | Increased coverage to 86%
    Fix warnings

--- lib/src/test/kotlin/trees/rbtest/RBTest.kt
+++ lib/src/test/kotlin/trees/rbtest/RBTest.kt
@@ -11,7 +11,7 @@ class RBTest {
 
     @BeforeEach
     fun setup() {
-        rbTree = RBTree<Int, Int>()
+        rbTree = RBTree()
     }
 
     @Test
@@ -20,8 +20,22 @@ class RBTest {
         for (i in array) {
             rbTree[i] = i
         }
-        val expectedGet: Array<Int?> = Array(5, { i -> i })
-        val actualGet: Array<Int?> = Array(5, { i -> rbTree.get(i) })
+        val expectedGet: Array<Int?> = Array(5) { i -> i }
+        val actualGet: Array<Int?> = Array(5) { i -> rbTree[i] }
+        assertContentEquals(expectedGet, actualGet, "Get method must return the value associated with the key")
+        val expectedSize = 5
+        val actualSize = rbTree.size
+        assertEquals(expectedSize, actualSize, "Size the tree must correspond to the number key-value pairs")
+    }
+
+    @Test
+    fun `array test`() {
+        val array = intArrayOf(2, 1, 3, 0, 4)
+        for (i in array) {
+            rbTree[i] = i
+        }
+        val expectedGet: Array<Int?> = Array(5) { i -> i }
+        val actualGet: Array<Int?> = Array(5) { i -> rbTree[i] }
         assertContentEquals(expectedGet, actualGet, "Get method must return the value associated with the key")
         val expectedSize = 5
         val actualSize = rbTree.size
@@ -49,16 +63,16 @@ class RBTest {
     @Test
     fun `simple array set test`() {
         for (i in 0..19) {
-            rbTree.set(i, i)
+            rbTree[i] = i
         }
 
         for (i in 0..19) {
-            val expectedContains = rbTree.get(i)
+            val expectedContains = rbTree[i]
             assertEquals(expectedContains, i, "Added and not removed element does not contain in the tree")
         }
 
-        val expectedGet: Array<Int?> = Array(20, { i -> i })
-        val actualGet: Array<Int?> = Array(20, { i -> rbTree.get(i) })
+        val expectedGet: Array<Int?> = Array(20) { i -> i }
+        val actualGet: Array<Int?> = Array(20) { i -> rbTree[i] }
         assertContentEquals(expectedGet, actualGet, "Get method must return the value associated with the key")
         val expectedSize = 20
         val actualSize = rbTree.size
@@ -68,22 +82,22 @@ class RBTest {
     @Test
     fun `simple array set remove test`() {
         for (i in 0..19) {
-            rbTree.set(i, i)
+            rbTree[i] = i
         }
         for (i in 10..19) {
             rbTree.remove(i)
         }
         for (i in 10..19) {
-            val expectedNotContains = rbTree.get(i)
+            val expectedNotContains = rbTree[i]
             assertEquals(expectedNotContains, null, "Removed element contains in the tree")
         }
         for (i in 0..9) {
-            val expectedContains = rbTree.get(i)
+            val expectedContains = rbTree[i]
             assertEquals(expectedContains, i, "Added and not removed element does not contain in the tree")
         }
 
-        val expectedGet: Array<Int?> = Array(10, { i -> i })
-        val actualGet: Array<Int?> = Array(10, { i -> rbTree.get(i) })
+        val expectedGet: Array<Int?> = Array(10) { i -> i }
+        val actualGet: Array<Int?> = Array(10) { i -> rbTree[i] }
         assertContentEquals(expectedGet, actualGet, "Get method must return the value associated with the key")
         val expectedSize = 10
         val actualSize = rbTree.size
@@ -93,21 +107,48 @@ class RBTest {
     @Test
     fun `adding and removing all elements test`() {
         for (i in 0..19) {
-            rbTree.set(i, i)
+            rbTree[i] = i
         }
         for (i in 0..19) {
             rbTree.remove(i)
         }
         for (i in 0..19) {
-            val expectedNotContains = rbTree.get(i)
+            val expectedNotContains = rbTree[i]
             assertEquals(expectedNotContains, null, "Removed element contains in the tree")
         }
 
         val expectedGet: Array<Int?> = emptyArray()
-        val actualGet: Array<Int?> = Array(rbTree.size, { i -> rbTree.get(i) })
+        val actualGet: Array<Int?> = Array(rbTree.size) { i -> rbTree[i] }
         assertContentEquals(expectedGet, actualGet, "Get method must return the value associated with the key")
         val expectedSize = 0
         val actualSize = rbTree.size
         assertEquals(expectedSize, actualSize, "Size the tree must correspond to the number key-value pairs")
+    }
+
+    @Test
+    fun `rotate left`() {
+        val keys = intArrayOf(0, -1, 4, 2, 6, -2, 1, 3, 5, 7)
+        for (key in keys) rbTree[key] = key
+        rbTree.remove(-2)
+
+        val expectedResult = arrayOf(0, -1, 4, 2, 6, null, 1, 3, 5, 7)
+        val actualResult = Array(keys.size) { i -> rbTree[keys[i]] }
+        assertContentEquals(
+            expectedResult,
+            actualResult,
+        )
+    }
+
+    @Test
+    fun `rotate right`() {
+        val keys = intArrayOf(0, 1, -1, -3, -2, -4)
+        for (key in keys) rbTree[key] = key
+
+        val expectedResult = Array<Int?>(keys.size) { i -> keys[i] }
+        val actualResult = Array(keys.size) { i -> rbTree[keys[i]] }
+        assertContentEquals(
+            expectedResult,
+            actualResult,
+        )
     }
 }
